2ddaf7b3a8833f847e9ae14effd8e6b3521e219e
Neerfix <nicolas.notararigo@ynov.com>
2018-10-11 10:43:39 +0200

    last commit

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -1,9 +1,11 @@
 <?php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use App\Entity\User;
 use App\Form\RegistrationType;
 
@@ -12,22 +14,32 @@ class RegistrationController extends AbstractController
     /**
      * @Route("/inscription", name="registration")
      */
-    public function index(Request $request)
+    public function index(Request $request, UserPasswordEncoderInterface $encoder)
     {
         $user = new User();
+
         $form = $this->createForm(RegistrationType::class, $user);
         $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
             $user = $form->getData();
+
+            $plainPassword = $user->getPassword();
+            $encryptedPassword = $encoder->encodePassword($user, $plainPassword);
+            $user->setPassword($encryptedPassword);
+
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
+
             return $this->redirectToRoute('registration_success');
         }
+
         return $this->render('user/registration.html.twig', [
             'form' => $form->createView(),
         ]);
     }
+
     /**
      * @Route("/inscription/ok", name="registration_success")
      */
--- src/Migrations/Version20181010152156.php
+++ src/Migrations/Version20181010152156.php
@@ -8,7 +8,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20181009102248 extends AbstractMigration
+final class Version20181010152156 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
@@ -17,6 +17,7 @@ final class Version20181009102248 extends AbstractMigration
 
         $this->addSql('CREATE TABLE boat (id INT AUTO_INCREMENT NOT NULL, category_id INT NOT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, picture VARCHAR(255) NOT NULL, price INT NOT NULL, materials VARCHAR(255) NOT NULL, length DOUBLE PRECISION NOT NULL, width DOUBLE PRECISION NOT NULL, berths INT NOT NULL, propulsion VARCHAR(255) NOT NULL, speed DOUBLE PRECISION NOT NULL, power INT NOT NULL, heliport INT NOT NULL, created_year VARCHAR(4) NOT NULL, nb_views INT NOT NULL, INDEX IDX_D86E834A12469DE2 (category_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:simple_array)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE boat ADD CONSTRAINT FK_D86E834A12469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
     }
 
@@ -28,5 +29,6 @@ final class Version20181009102248 extends AbstractMigration
         $this->addSql('ALTER TABLE boat DROP FOREIGN KEY FK_D86E834A12469DE2');
         $this->addSql('DROP TABLE boat');
         $this->addSql('DROP TABLE category');
+        $this->addSql('DROP TABLE user');
     }
 }
